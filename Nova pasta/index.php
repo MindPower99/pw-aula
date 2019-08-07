3b2a3e3e8b5a4d104d7c25ed65d62f58aa8c4897
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-08-07 04:27:46 -0300

    fazendo a conexão com o banco

--- Nova pasta/index.php
+++ Nova pasta/index.php
@@ -1,11 +1,27 @@
+
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "etec";
 
-$conn = new mysqli('localhost', 'root', '', 'alunoetec');
- 
-if($conn->connect error) {
-	die('Connection failed: " .$conn->connect error);
-}
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-$sql = "SELECT nome, senha from aluno";
-
+$sql = "SELECT id,nome, senha FROM aluno";
 $result = $conn->query($sql);
+
+echo "O valor de nome é: " . $_POST["nome"] . "<br>"; ; 
+echo "<br>O valor de senha  é: " . $_POST["senha"] . "<br>"; 
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Nome: " . $row["nome"]. " " . $row["senha"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
