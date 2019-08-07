91d2844db0f4f4d60c511880f70af84f582bc81a
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-08-07 03:55:48 -0300

    fazendo a conexão com o banco

--- Nova pasta/index.php
+++ Nova pasta/index.php
@@ -1,1 +1,11 @@
-sdfad
+<?php
+
+$conn = new mysqli('localhost', 'root', '', 'alunoetec');
+ 
+if($conn->connect error) {
+	die('Connection failed: " .$conn->connect error);
+}
+
+$sql = "SELECT nome, senha from aluno";
+
+$result = $conn->query($sql);
